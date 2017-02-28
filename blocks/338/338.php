a66a317e09147a2817a31f77b66374bf1269277e
MinhChau <pemidolly@gmail.com>
2017-02-28 07:20:46 +0700

    Template 338

--- blocks/338/338.php
+++ blocks/338/338.php
@@ -23,12 +23,12 @@
                 <div class="navbar-collapse" id="catenav">
                     <ul class="nav navbar-nav">
                         <li><h4><a href="#" style="color: #000">BÀI VIẾT CHUYÊN ĐỀ</a>
-                                <div class="clear"></div>
-                            </h4><span style="padding-left: 15px;"><i class="fa fa-angle-right"></i></span></li>
-                        <li class="col-xs-6"><a href="#">Tin học cơ sở</a><div class="clear"></div></li>
-                        <li class="col-xs-6"><a href="#">Công nghệ phần mềm</a><div class="clear"></div></li>
-                        <li class="col-xs-6"><a href="#">Đồ họa</a><div class="clear"></div></li>
-                        <li class="col-xs-6"><a href="#">Mạng máy tính</a><div class="clear"></div></li>
+                                
+                            </h4><span style="padding-left: 15px; font-size: 0px;"><i class="fa fa-angle-right"></i></span></li>
+                        <li class="col-xs-6 auto-width"><a href="#">Tin học cơ sở</a></li>
+                        <li class="col-xs-6 auto-width"><a href="#">Công nghệ phần mềm</a></li>
+                        <li class="col-xs-6 auto-width"><a href="#">Đồ họa</a></li>
+                        <li class="col-xs-6 auto-width"><a href="#">Mạng máy tính</a></li>
                     </ul>
                 </div>
                 <!--END SPECIAL SUBJECT-->
