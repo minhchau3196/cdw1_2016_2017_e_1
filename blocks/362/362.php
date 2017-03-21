4e6a1b49081d46f59c93c2ded38fde37b05c70e0
Thanh Trúc <maithithanhtruc.19@gmail.com>
2017-03-21 10:17:01 +0700

    template webthem
    362

---
+++ blocks/362/362.php
@@ -0,0 +1,52 @@
+﻿
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">       
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/362.less', 'css/362.css');
+        ?>
+        <link href="css/362.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+        <div class="type-362">
+            <section class="" style="background-color:#f4f5f8;padding-top:30px;padding-bottom:30px;">
+                <div class="container">
+                    <div class="row" style="">
+                        <div class="col-md-2">
+                            <h4 class="subtitle" style="margin: 0; line-height: 48px;">Quick Links</h4>
+                        </div>
+                        <div class="col-md-2">
+                            <a href="#" class="btn btn-secondary btn-normal btn-block">Careers</a>
+                        </div>
+                        <div class="col-md-2">
+                            <a href="#" class="btn btn-secondary btn-normal btn-block">Contact</a>
+                        </div>
+                        <div class="col-md-2">
+                            <a href="#" class="btn btn-secondary btn-normal btn-block">Market Info</a>
+                        </div>
+                        <div class="col-md-2">
+                            <a href="#" class="btn btn-secondary btn-normal btn-block">Technology</a>
+                        </div>
+                        <div class="col-md-2">
+                            <a href="#" class="btn btn-secondary btn-normal btn-block">Latest News</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
---
+++ blocks/363/363.php
@@ -0,0 +1,66 @@
+﻿
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">       
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/363.less', 'css/363.css');
+        ?>
+        <link href="css/363.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+        <div class="type-363">
+            <section class="" style="background-color:#f4f5f8;padding-top:30px;padding-bottom:30px;">
+                <div class="container">
+                    <div class="row center-block-sm" style="">
+                        <div class="col-md-3">
+                            <img src="images/hinh/globe.png" class="counter-img" alt="counter">
+                            <div class="stats-info">
+                                <h4 class="counter">26</h4>
+                                <p>Offices Worldwide</p>
+
+                            </div>
+                        </div>
+                        <div class="col-md-3" style="border-left:1px solid #dee0e5;">
+                            <img src="images/hinh/friends.png" class="counter-img" alt="counter">
+                            <div class="stats-info">
+                                <h4 class="counter">10000</h4>
+                                <p>Satisfied Employees</p>
+
+                            </div>
+                        </div>
+                        <div class="col-md-3" style="border-left:1px solid #dee0e5;">
+                            <img src="images/hinh/fire.png" class="counter-img" alt="counter">
+                            <div class="stats-info">
+                                <h4 class="counter">126</h4>
+                                <p>Refineries &amp; Operations</p>
+
+                            </div>
+                        </div>
+                        <div class="col-md-3" style="border-left:1px solid #dee0e5;">
+                            <img src="images/hinh/badge.png" class="counter-img" alt="counter">
+                            <div class="stats-info">
+                                <h4 class="counter">35</h4>
+                                <p>Awards &amp; Recognitions</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
