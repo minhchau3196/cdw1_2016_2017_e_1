c92f0cc05c9085c41ce2e886bc74b160b1fc0786
MaiLoan <minhanhlovely95@gmail.com>
2017-03-21 09:24:08 +0700

    offshore
    370-371

---
+++ blocks/371/371.php
@@ -0,0 +1,54 @@
+<html lang="en" class="no-js"><head>
+        <meta charset="utf-8">
+        <title>Industrial Business Responsive WP theme - Offshore Preview - ThemeForest</title>
+        <meta name="description" content="&amp;lt;h3&amp;gt;&amp;lt;strong&amp;gt;Offshore&amp;lt;/strong&amp;gt; is a well designed Industrial Website Theme with ...">
+        <meta name="robots" content="noindex, follow">
+        <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+        <meta name="generator" content="Powered by Visual Composer - drag and drop page builder for WordPress.">
+
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="ionicons.min.css" rel="stylesheet" type="text/css"/>
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/styles.less', 'css/styles.css');
+        ?>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="type-11-1">
+            <div class="tp-fullwidth-forcer"><!-- END REVOLUTION SLIDER -->            <!-- Main Container -->
+                <div class="full-width">    <div class="row">
+
+
+                        <div class="col-md-12">
+
+
+                            <section class="" style=""><div class="container"><div class="row center-block-sm" style=""><div class="col-md-5">
+                                            <span class="label label-default" style="&quot;no&quot;">THE COMPANY</span>
+                                            <h4 style="font-size: 18px; font-weight: bold; line-height: 28px; color: #093eb9; margin-top: 20px; margin-bottom: 20px;">Interactively empower diverse imperatives after prospective convergence.</h4>
+                                            <p>Assertively productize efficient partnerships through customer directed supply chains. Continually maintain process-centric catalysts for change via backward compatible value.</p>
+                                            <a href="#" id="btn-1168-737" class="btn btn-primary">READ OUR MISSION</a>
+                                        </div><div class="col-md-7">
+                                            <div class="company-image">
+                                                <div class="img-left hover-effect">
+                                                    <img src="images/company-image2.jpg" width="50" height="30" alt="Company Image">
+                                                </div>
+                                                <div class="img-right hover-effect">
+                                                    <img src="images/company-image1.jpg" alt="Company Image">
+                                                </div>
+                                            </div>
+                                        </div></div></div></section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
