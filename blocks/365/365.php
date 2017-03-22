840640dd1aa4c044244d5cb1003728d640780ca0
ThanhTruc-1995 <maithithanhtruc.19@gmail.com>
2017-03-22 21:15:12 +0700

    template footer
    365

--- blocks/365/365.php
+++ blocks/365/365.php
@@ -29,61 +29,42 @@
                         <div class="col-sm-3 footer_block footer_block__1">
                             <h3>Information</h3>
                             <ul class="footer_links">
-
-                                <li class="active"><a href="/" title="">Home</a></li>
-
-                                <li><a href="/collections" title="">Collection</a></li>
-
-                                <li><a href="/search" title="">Search</a></li>
-
-                                <li><a href="/blogs/blog" title="">Blog</a></li>
-
-                                <li><a href="/pages/about-us" title="">About us</a></li>
-
-                                <li><a href="/pages/contact-us" title="">Contact us</a></li>
-
-                                <li><a href="/pages/sitemap" title="">Sitemap</a></li>
-
+                                <li class="active"><a href="#" title="">Home</a></li>
+                                <li><a href="#" title="">Collection</a></li>
+                                <li><a href="#" title="">Search</a></li>
+                                <li><a href="#" title="">Blog</a></li>
+                                <li><a href="#" title="">About us</a></li>
+                                <li><a href="#" title="">Contact us</a></li>
+                                <li><a href="#" title="">Sitemap</a></li>
                             </ul>
                         </div>
 
-                        <div class="col-sm-3 footer_block footer_block__2">
+                        <div class="col-md-3 col-sm-3 footer_block footer_block__2">
                             <h3>Why buy from us</h3>
                             <ul class="footer_links">
-
-                                <li><a href="/blogs/blog" title="">Shipping &amp; Returns</a></li>
-
-                                <li><a href="/pages/about-us" title="">Secure Shopping</a></li>
-
-                                <li><a href="/pages/contact-us" title="">International Shipping</a></li>
-
-                                <li><a href="/pages/sitemap" title="">Affiliates</a></li>
-
-                                <li><a href="/collections" title="">Group Sales</a></li>
-
+                                <li><a href="#" title="">Shipping &amp; Returns</a></li>
+                                <li><a href="#" title="">Secure Shopping</a></li>
+                                <li><a href="#" title="">International Shipping</a></li>
+                                <li><a href="#" title="">Affiliates</a></li>
+                                <li><a href="#" title="">Group Sales</a></li>
                             </ul>
                         </div>
 
-                        <div class="col-sm-3 footer_block footer_block__3">
+                        <div class="col-md-3 col-sm-3 footer_block footer_block__3">
                             <h3>My account</h3>
                             <ul class="footer_links">
-
                                 <li><a href="/account" title="">My Account</a></li>
-
                                 <li><a href="/account/addresses" title="">My Addresses</a></li>
-
                                 <li><a href="/cart" title="">My Cart</a></li>
-
                             </ul>
                         </div>          
 
-                        <div class="col-sm-3 footer_block footer_block__4">
+                        <div class="col-md-3 col-sm-3 footer_block footer_block__4">
                             <h3>Contacts</h3>
                             <ul class="footer_contacts">
-                                <li class="contacts_company">My Company</li>
-                                <li class="contacts_address">Glasgow D04 89GR</li>
-                                <li class="contacts_phone"><div>Tel:</div> <a href="tel:1-800-1234-567">1-800-1234-567</a></li>
-                                <li class="contacts_email"><a href="mailto:info@demolink.org">info@demolink.org</a></li>
+                                <li class="contacts_company">My Company Glasgow D04 89GR</li>
+                                <li class="contacts_phone"><div>Tel:</div> <a href="#">1-800-1234-567</a></li>
+                                <li class="contacts_email"><a href="#">info@demolink.org</a></li>
                             </ul>
                             <h3>Follow us</h3>
 
@@ -97,6 +78,7 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="copyright">
                     <div class="container">
                         <p role="contentinfo">© 2017 Raphael. All rights reserved.<a href="#">Powered by Shopify</a></p>
