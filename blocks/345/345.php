e96353190a2e6ef578b459d0760a2ebb93cfff6e
Thanh Truc <maithithanhtruc.19@gmail.com>
2017-04-03 17:21:05 +0700

    module367,366,345
    edit css.html

--- blocks/345/345.php
+++ blocks/345/345.php
@@ -29,48 +29,42 @@
                 <div class="darker-row">
                     <div class="container">
                         <div class="row">
-                            <div class=" col-md-6 col-sm-12 col-xs-12">
-                                <div class="span4">
-                                    <div class="higher-line">
-                                        Welcome! Please
-                                        <a href="#registerModal" role="button" data-toggle="modal">Register</a> or
-                                        <a href="#loginModal" role="button" data-toggle="modal">Login</a>
-                                    </div>
+                            <div class="span4">
+                                <div class="higher-line">
+                                    Wellcome! Please
+                                    <a href="#registerModal" role="button" data-toggle="modal">Register</a> or
+                                    <a href="#loginModal" role="button" data-toggle="modal">Login</a>
                                 </div>
                             </div>
-                            <div class=" col-md-6 col-sm-12 col-xs-12">
-                                <div class="span8">
-                                    <div class="topmost-line">
-                                        <div class="higher-line">
-                                            <a href="#" class="gray-link">My account</a>
-                                            &nbsp; | &nbsp;
-                                            <a href="#" class="gray-link">Wishlist (2)</a>
-                                            &nbsp; | &nbsp;
-                                            <a href="#" class="gray-link">Checkout</a>
+                            <div class="span8">
+                                <div class="topmost-line">
+                                    <div class="higher-line">
+                                        <a href="#" class="gray-link">My account</a>
+                                        &nbsp; | &nbsp;
+                                        <a href="#" class="gray-link">Wishlist (2)</a>
+                                        &nbsp; | &nbsp;
+                                        <a href="#" class="gray-link">Checkout</a>
+                                    </div>
+                                    &nbsp;
+                                    <div class="lang-currency">
+                                        <div class="dropdown js-selectable-dropdown">
+                                            <a data-toggle="dropdown" class="selected" href="#"><span class="js-change-text"><i class="famfamfam-flag-gb"></i> English (EN)</span> <b class="caret"></b></a>
+                                            <ul class="dropdown-menu js-possibilities" role="menu" aria-labelledby="dLabel">
+                                                <li><a href="#"><i class="famfamfam-flag-gb"></i> English (EN)</a></li>
+                                                <li><a href="#"><i class="famfamfam-flag-si"></i> Slovenian (SI)</a></li>
+                                                <li><a href="#"><i class="famfamfam-flag-de"></i> German (DE)</a></li>
+                                                <li><a href="#"><i class="famfamfam-flag-fr"></i> French (FR)</a></li>
+                                                <li><a href="#"><i class="famfamfam-flag-es"></i> Spanish (ES)</a></li>
+                                            </ul>
                                         </div>
-                                        &nbsp;
-                                        <div class="lang-currency">
-                                            <div class="dropdown">
-                                                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><i class="famfamfam-flag-gb"></i> English (EN)
-                                                </button>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="#"><i class="famfamfam-flag-gb"></i> English (EN)</a></li>
-                                                    <li><a href="#"><i class="famfamfam-flag-si"></i> Slovenian (SI)</a></li>
-                                                    <li><a href="#"><i class="famfamfam-flag-de"></i> German (DE)</a></li>
-                                                    <li><a href="#"><i class="famfamfam-flag-fr"></i> French (FR)</a></li>
-                                                    <li><a href="#"><i class="famfamfam-flag-es"></i> Spanish (ES)</a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="dropdown">
-                                                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">USD ($)
-                                                </button>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="#">USD ($)</a></li>
-                                                    <li><a href="#">EUR (€)</a></li>
-                                                    <li><a href="#">YEN (¥)</a></li>
-                                                    <li><a href="#">GBP (£)</a></li>
-                                                </ul>
-                                            </div>
+                                        <div class="dropdown js-selectable-dropdown">
+                                            <a data-toggle="dropdown" class="selected" href="#"><span class="js-change-text">USD ($)</span> <b class="caret"></b></a>
+                                            <ul class="dropdown-menu js-possibilities" role="menu" aria-labelledby="dLabel">
+                                                <li><a href="#">USD ($)</a></li>
+                                                <li><a href="#">EUR (€)</a></li>
+                                                <li><a href="#">YEN (¥)</a></li>
+                                                <li><a href="#">GBP (£)</a></li>
+                                            </ul>
                                         </div>
                                     </div>
                                 </div>
@@ -78,32 +72,27 @@
                         </div>
                     </div>
                 </div>
-                <!--LoGo- container-->
                 <div class="container">
                     <div class="row">
-                        <div class=" col-md-6 col-sm-6 col-xs-12">
-                            <div class="span7">
-                                <a class="brand" href="#">
-                                    <img src="images/logo.png" alt="Webmarket Logo" width="48" height="48">
-                                    <span class="pacifico">Web market</span>
-                                    <span class="tagline">Really Cool e-Commerce HTML Template</span>
-                                </a>
-                            </div>
+                        <div class="span7">
+                            <a class="brand" href="#">
+                                <img src="images/logo.png" alt="Webmarket Logo" width="48" height="48">
+                                <span class="pacifico">Webmarket</span>
+                                <span class="tagline">Really Cool e-Commerce HTML Template</span>
+                            </a>
                         </div>
-                        <div class=" col-md-6 col-sm-6 col-xs-12">
-                            <div class="span5">
-                                <div class="top-right">
-                                    <div class="icons">
-                                        <a href="#"><span class="fa fa-facebook"></span></a>
-                                        <a href="#"><span class="fa fa-skype"></span></a>
-                                        <a href="#"><span class="fa fa-twitter"></span></a>
-                                        <a href="#"><span class="fa fa-rss"></span></a>
-                                        <a href="#"><span class="fa fa-wordpress"></span></a>
-                                        <a href="#"><span class="fa fa-android"></span></a>
-                                        <a href="#"><span class="fa fa-html5"></span></a>
-                                        <a href="#"><span class="fa fa-windows"></span></a>
-                                        <a href="#"><span class="fa fa-apple"></span></a>
-                                    </div>
+                        <div class="span5">
+                            <div class="top-right">
+                                <div class="icons">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-wordpress" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-android" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-html5" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-windows" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-apple" aria-hidden="true"></i></a>
                                 </div>
                             </div>
                         </div>  
