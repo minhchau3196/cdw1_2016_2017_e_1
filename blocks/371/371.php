5420e42b9d8eb5faa1eb7c9161ca24c1aec7d1e4
MaiLoan <minhanhlovely95@gmail.com>
2017-04-03 17:13:04 +0700

    chỉnh css

--- blocks/371/371.php
+++ blocks/371/371.php
@@ -1,58 +1,49 @@
 <html lang="en" class="no-js"><head>
         <meta charset="utf-8">
-        <title>Industrial Business Responsive WP theme - Offshore Preview - ThemeForest</title>
-        <meta name="description" content="&amp;lt;h3&amp;gt;&amp;lt;strong&amp;gt;Offshore&amp;lt;/strong&amp;gt; is a well designed Industrial Website Theme with ...">
-        <meta name="robots" content="noindex, follow">
-        <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
-        <meta name="generator" content="Powered by Visual Composer - drag and drop page builder for WordPress.">
+        <title>Offshore</title>        
 
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <link href="ionicons.min.css" rel="stylesheet" type="text/css"/>
 
         <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
         }
         $less = new lessc;
-        $less->compileFile('less/styles.less', 'css/styles.css');
+        $less->compileFile('less/371.less', 'css/371.css');
         ?>
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="css/371.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <div class="type-11-1">
-            <div class="tp-fullwidth-forcer"><!-- END REVOLUTION SLIDER -->            <!-- Main Container -->
-                <div class="full-width">    
-                    <div class="row">
-                        <div class="col-md-12">
-                            <section class="" style="">
-                                <div class="container">
-                                    <div class="row center-block-sm" style="">
-                                        <div class="col-md-5 col-sm-12 col-lg-5">
-                                            <span class="label-default" style="&quot;no&quot;">THE COMPANY</span>
-                                            <h4 style="font-size: 18px; font-weight: bold; line-height: 28px; color: #093eb9; margin-top: 20px; margin-bottom: 20px;">Interactively empower diverse imperatives after prospective convergence.</h4>
-                                            <p>Assertively productize efficient partnerships through customer directed supply chains. Continually maintain process-centric catalysts for change via backward compatible value.</p>
-                                            <a href="#" id="btn-1168-737" class="btn btn-primary">READ OUR MISSION</a>
-                                        </div>
-                                        <div class="col-md-7 col-sm-12 col-lg-7">
-                                            <div class="company-image">
-                                                <div class="img-left hover-effect">
-                                                    <img src="images/company-image2.jpg" width="50" height="30" alt="Company Image">
-                                                </div>
-                                                <div class="img-right hover-effect">
-                                                    <img src="images/company-image1.jpg" alt="Company Image">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+        <div class="type-371">            
+            <section>
+                <div class="container">
+                    <div class="row center-block-sm">
+                        <div class="col-md-5 col-sm-12 col-xs-12">
+                            <span class="label-default" style="&quot;no&quot;">THE COMPANY</span>
+                            <h4>Interactively empower diverse imperatives after prospective convergence.</h4>
+                            <p>Assertively productize efficient partnerships through customer directed supply chains.
+                                Continually maintain process-centric catalysts for change via backward compatible value.</p>
+                            <div class="btn1">
+                                <a href="#" class="btn-primary">READ OUR MISSION</a>
+                            </div>
+                        </div>
+                        <div class="col-md-7 col-sm-12 col-xs-12">
+                            <div class="company-image">
+                                <div class="img-left hover-effect">
+                                    <img src="images/company-image2.jpg" alt="Company Image">
                                 </div>
-                            </section>
+                                <div class="img-right hover-effect">
+                                    <img src="images/company-image1.jpg" alt="Company Image">
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </div>
+
     </body>
 </html>
--- blocks/372/372.php
+++ blocks/372/372.php
@@ -5,40 +5,42 @@
 
 <head>
     <meta charset="UTF-8" />
-    <script src="catalog/view/javascript/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
-    <link href="catalog/view/javascript/bootstrap-3.3.7-dist/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-    <script src="catalog/view/javascript/bootstrap-3.3.7-dist/js/bootstrap.min.js" type="text/javascript"></script>
-    <link href="font-awesome.min.css" rel="stylesheet" type="text/css"/>
-    <link href="ionicons.min.css" rel="stylesheet" type="text/css"/>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
     <?php
             if (!class_exists('lessc')) {
                 include ('./libs/lessc.inc.php');
             }
             $less = new lessc;
-            $less->compileFile('less/type.less', 'css/type.css');
+            $less->compileFile('less/372.less', 'css/372.css');
         ?>
-    <link href="css/type.css" rel="stylesheet" type="text/css"/>
+    <link href="css/372.css" rel="stylesheet" type="text/css"/>
 </head>
 
 <body>
-   <div class="type-38-1">
-    <section class="" style="">
+   <div class="type-372">
+    <section>
         <div class="container">
-            <div class="row center-block-sm" style="">
-                <div class="col-md-4">
-                    <div class="hover-effect"><img src="image/services-large-409x332-1.jpg" title="" alt="TECHNOLOGY &amp; INNOVATION" style="max-height:241px;"></div>
+            <div class="row center-block-sm">
+                <div class="col-md-4 col">
+                    <div class="hover-effect">
+                        <img src="image/services-two-409x272-1.jpg" title="" alt="TECHNOLOGY &amp; INNOVATION" style="max-height:241px;">
+                    </div>
                     <h4 class="services-title-one subtitle">TECHNOLOGY &amp; INNOVATION</h4>
-                    <p>Uniquely transform premier infrastructures before functional metrics. Completely mesh sustainable leadership for economically sound sources.â€¦</p>
-                    <a class="link" href="#">Read More</a></div>
-                <div class="col-md-4">
+                    <p>Uniquely transform premier infrastructures before functional metrics.
+                        Completely mesh sustainable leadership for economically sound sources....</p>
+                    <a class="link" href="#">Read More</a>
+                </div>
+                <div class="col-md-4 col">
                     <div class="hover-effect"><img src="image/services-three-409x272-1.jpg" title="" alt="OUR OPERATIONS" style="max-height:241px;"></div>
                     <h4 class="services-title-one subtitle">OUR OPERATIONS</h4>
-                    <p>Uniquely transform premier infrastructures before functional metrics. Completely mesh sustainable leadership for economically sound sources.â€¦</p>
+                    <p>Uniquely transform premier infrastructures before functional metrics. 
+                        Completely mesh sustainable leadership for economically sound sources....</p>
                     <a class="link" href="#">Read More</a></div>
-                <div class="col-md-4">
+                <div class="col-md-4 col">
                     <div class="hover-effect"><img src="image/services-two-409x272-1.jpg" title="" alt="SOCIAL RESPONIBILITY" style="max-height:241px;"></div>
                     <h4 class="services-title-one subtitle">SOCIAL RESPONIBILITY</h4>
-                    <p>Uniquely transform premier infrastructures before functional metrics. Completely mesh sustainable leadership for economically sound sources.â€¦</p>
+                    <p>Uniquely transform premier infrastructures before functional metrics. 
+                        Completely mesh sustainable leadership for economically sound sources....</p>
                     <a class="link" href="#">Read More</a></div>
                 
             </div>
