421a284080b0263baa0f83dd409945bfb5323275
Thanh Truc <maithithanhtruc.19@gmail.com>
2017-04-10 17:20:38 +0700

    module 393,394
    edit css,html

---
+++ blocks/394/394.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/394.less', 'css/394.css');
+        ?>
+        <link href="css/394.css" rel="stylesheet" type="text/css"/>
+
+    </head>
+
+    <body>
+        <div class="type-394">
+            <div class="container">
+                <div class="row" style="">
+                    <div class="col-md-7"> </div>                  
+                    <div class="col-md-5">
+                        <div class="publication-downloads">
+                            <h2 class="publish">Publications</h2><div class="download-file">
+                                <a href="#" class="offshore-download"><i class="fa fa-file-pdf-o" style="margin-right:5px;" aria-hidden="true"></i>Download PDF<span>17.18 KB</span></a>
+                            </div>
+                            <ul class="download-list list-unstyled">
+                                <li class="download-subtitle"><p>Other Downloads</p></li>
+                                <li><a href="#">Annual Report</a><span>40.79 KB</span></li>
+                                <li><a href="#">Sustainability Report</a><span>35.35 KB</span></li>
+                                <li><a href="#">Statistical Report</a><span>30.72 KB</span></li>
+                                <li><a href="#">Energy Outlook</a><span>29.56 KB</span></li>
+                                <li><a href="#">Chairman's Message</a><span>17.18 KB</span></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
