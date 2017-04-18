ced97c84b904d7c8c5c473f1ac5d0fde4fd44098
ThanhTruc <maithithanhtruc.19@gmail.com>
2017-04-18 11:26:27 +0700

    module 383
    css.html

--- blocks/383/383.php
+++ blocks/383/383.php
@@ -31,17 +31,22 @@
                             <h1 class="page_heading">Contact us</h1>
                             <!--  GOOGLE MAP -->
                             <div id="map-canvas">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.4805347001443!2d106.7560762145813!3d10.851008692270929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752797e321f8e9%3A0xb3ff69197b10ec4f!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEPDtG5nIG5naOG7hyBUaOG7pyDEkOG7qWM!5e0!3m2!1svi!2s!4v1488176391234" width="100%" height="450px" frameborder="0" style="border:2" allowfullscreen></iframe> 
+                                <iframe
+                                    width="600"
+                                    height="450"
+                                    frameborder="0" style="border:0"
+                                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCqQIChJ72VoZuDVkWLBMUDtr5kp0B4u5E
+                                    &q=Space+Needle,Seattle+WA" allowfullscreen>
+                                </iframe>          
                             </div>                           
                         </div>
                         <div class="rte">
                             <p style="font-size:12px;">You can contact us any way that is convenient for you. We are available 24/7 via fax, email or telephone. You can also use a quick contact form below or visit our office personally.</p>
                             <p style="font-size:12px;">Email us with any questions or inquiries or use our contact data. We would be happy to answer your questions.</p>
-                        </div>
+                        </div>                     
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
